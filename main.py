634e8f432aa5883a9b1540abab2618f2607c01d1
InnesWarwick <inneswarwick@gmail.com>
2023-03-07 15:25:43 +0000

    modified:   main.py

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ from time import sleep
 from selenium.webdriver.common.keys import Keys
 import random
 import string
+#comment list
 comments = ["Classic 48224fb","love it 48224fb","this is my favourtie 48224fb post ever!"]
+
 def main():
     browser = webdriver.Firefox(executable_path="/usr/bin/firefox")
     browser.get('https://www.instagram.com/')
     sleep(5)
     browser.close()
+
 
 
 
