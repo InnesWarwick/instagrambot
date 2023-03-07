43ceba0a9d8c54c08aa1888b58712eadbc6a0a8a
InnesWarwick <inneswarwick@gmail.com>
2023-03-07 15:23:43 +0000

    new file:   main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+
+from selenium import webdriver
+from time import sleep
+from selenium.webdriver.common.keys import Keys
+import random
+import string
+comments = ["Classic 48224fb","love it 48224fb","this is my favourtie 48224fb post ever!"]
+def main():
+    browser = webdriver.Firefox(executable_path="/usr/bin/firefox")
+    browser.get('https://www.instagram.com/')
+    sleep(5)
+    browser.close()
+
+
+
+
+if __name__ == "__main__":
+    main()
